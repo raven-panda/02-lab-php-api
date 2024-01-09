5dcba47d21a8ba3d806de6b6146df7736c257c0b
Ambroise Maire <ambroise.maire7@gmail.com>
2024-01-09 09:37:04 +0100

    update: now checking if category exists for get requests

--- api/src/_lib/categories/category-by-id.php
+++ api/src/_lib/categories/category-by-id.php
@@ -12,94 +12,107 @@
         $mysql_connection = databaseConnection();
 
         if ($mysql_connection) {
-            
-            // Selecting the category with its attached technologies in the database
-            $sql_selectCategories = "SELECT t.name, t.ressources, t.categories, t.icon
-                FROM categories AS c
-                LEFT JOIN cat_tech AS ct ON c.id = ct.cat_id
-                LEFT JOIN technologies AS t ON ct.tech_id = t.id
-                WHERE c.name = :category
-                GROUP BY c.name, t.id";
-            $sth = $mysql_connection->prepare($sql_selectCategories);
 
+            $sql_checkCategory = "SELECT id FROM categories WHERE name = :category";
+            $sth = $mysql_connection->prepare($sql_checkCategory);
             $sth->bindParam(':category', $cat_name, PDO::PARAM_STR);
-    
             $sth->execute();
 
-            $results = $sth->fetchAll(PDO::FETCH_ASSOC);
+            $cat_exists = $sth->fetch(PDO::FETCH_ASSOC);
+            unset($sth);
 
-            if (!empty($results)) {
+            if (!empty($cat_exists)) {
 
-                $response_data = $results;
+                // Selecting the category with its attached technologies in the database
+                $sql_selectCategories = "SELECT t.name, t.ressources, t.categories, t.icon
+                                        FROM categories AS c
+                                        JOIN cat_tech AS ct ON c.id = ct.cat_id
+                                        JOIN technologies AS t ON ct.tech_id = t.id
+                                        WHERE c.name = :category";
+                
+                $sth = $mysql_connection->prepare($sql_selectCategories);
 
-                // If the method is PUT, trigger the 'edit' script
-                if ($request_method === 'PUT') {
+                $sth->bindParam(':category', $cat_name, PDO::PARAM_STR); 
 
-                    // Retrieving and parsing the form-data
-                    $json_data = file_get_contents('php://input');
-                    $_PUT = parse_raw_http_request($json_data);
-    
-                    if (isset($_PUT['name']) && !empty($_PUT['name'])) {
+                $sth->execute();
 
-                        $new_name = strtolower(htmlspecialchars($_PUT['name']));
+                $results = $sth->fetchAll(PDO::FETCH_ASSOC);
+                
+                if (!empty($results)) {
+                    if ($request_method === "GET" && $results) $response_data = $results;
 
-                        // Checking if the category name sent contains only alphanumeric characters, dash and underscore
-                        if (preg_match('/^[a-z0-9_-]+$/', $new_name)) {
-    
-                            try {
-                                
-                                // Updating the category in the database
-                                $sql_updateCat = "UPDATE categories SET `name` = :new_name WHERE `name` = :old_name";
-                                $sth = $mysql_connection->prepare($sql_updateCat);
-                                
-                                $sth->bindParam(':old_name', $cat_name, PDO::PARAM_STR);
-                                $sth->bindParam(':new_name', $new_name, PDO::PARAM_STR);
-                                
-                                $sth->execute();
-        
-                                $row = $sth->rowCount();
-        
-                                // Checking if it changed anything to send the appropriate response
-                                http_response_code(200);
-        
-                            } catch (Exception $err) {
-                                error_log($err);
-                                http_response_code(500);
+                    // If the method is PUT, trigger the 'edit' script
+                    if ($request_method === 'PUT') {
+
+                        // Retrieving and parsing the form-data
+                        $json_data = file_get_contents('php://input');
+                        $_PUT = parse_raw_http_request($json_data);
+
+                        if (isset($_PUT['name']) && !empty($_PUT['name'])) {
+
+                            $new_name = strtolower(htmlspecialchars($_PUT['name']));
+
+                            // Checking if the category name sent contains only alphanumeric characters, dash and underscore
+                            if (preg_match('/^[a-z0-9_-]+$/', $new_name)) {
+
+                                try {
+                                    
+                                    // Updating the category in the database
+                                    $sql_updateCat = "UPDATE categories SET `name` = :new_name WHERE `name` = :old_name";
+                                    $sth = $mysql_connection->prepare($sql_updateCat);
+                                    
+                                    $sth->bindParam(':old_name', $cat_name, PDO::PARAM_STR);
+                                    $sth->bindParam(':new_name', $new_name, PDO::PARAM_STR);
+                                    
+                                    $sth->execute();
+            
+                                    $row = $sth->rowCount();
+            
+                                    // Checking if it changed anything to send the appropriate response
+                                    http_response_code(200);
+            
+                                } catch (Exception $err) {
+                                    error_log($err);
+                                    http_response_code(500);
+                                }
+
+                            } else {
+                                http_response_code(400);
                             }
 
                         } else {
                             http_response_code(400);
                         }
-    
-                    } else {
-                        http_response_code(400);
                     }
-                }
-    
-                // If the method is DELETE
-                if ($request_method === 'DELETE') {
-                    
-                    try {
 
-                        // Deleting the category from the database
-                        $sql_deleteCat = "DELETE FROM categories WHERE `name` = :name";
-                        $sth = $mysql_connection->prepare($sql_deleteCat);
+                    // If the method is DELETE
+                    if ($request_method === 'DELETE') {
                         
-                        $sth->bindParam(':name', $cat_name, PDO::PARAM_STR);
-                        
-                        $sth->execute();
+                        try {
 
-                        $row = $sth->rowCount();
-    
-                        // Checking if it changed anything to send the appropriate response
-                        http_response_code(200);
+                            // Deleting the category from the database
+                            $sql_deleteCat = "DELETE FROM categories WHERE `name` = :name";
+                            $sth = $mysql_connection->prepare($sql_deleteCat);
+                            
+                            $sth->bindParam(':name', $cat_name, PDO::PARAM_STR);
+                            
+                            $sth->execute();
 
-                    } catch (Exception $err) {
-                        error_log($err);
-                        http_response_code(500);
+                            $row = $sth->rowCount();
+
+                            // Checking if it changed anything to send the appropriate response
+                            http_response_code(200);
+
+                        } catch (Exception $err) {
+                            error_log($err);
+                            http_response_code(500);
+                        }
                     }
+
                 }
 
+            } else {
+                http_response_code(404);
             }
 
         } else {
